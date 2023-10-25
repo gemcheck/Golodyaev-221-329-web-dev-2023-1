0805dfd34b24fef19186aca1e06a47ec080ad73b
gemcheck <golodyaev2004@yandex.ru>
2023-10-25 21:53:54 +0300

    lab3

---
+++ labjs/getSortedArray.js
@@ -0,0 +1,10 @@
+function getSortedArray(array, key) {
+    // your code here
+}
+
+// Пример работы функции.
+// let array = [{name: 'Макар', age: 20}, {name: 'Роберт', age: 32}, {name: 'Екатерина', age: 50}, {name: 'Оксана', age: 24}, {name: 'Святослав', age: 43}];
+// array = getSortedArray(array, 'age')
+// console.log(array); // [{name: 'Макар', age: 20}, {name: 'Оксана', age: 24}, {name: 'Роберт', age: 32}, {name: 'Святослав', age: 43}, {name: 'Екатерина', age: 50}];
+// array = getSortedArray(array, 'name')
+// console.log(array); // [{name: 'Екатерина', age: 50}, {name: 'Макар', age: 20}, {name: 'Оксана', age: 24}, {name: 'Роберт', age: 32}, {name: 'Святослав', age: 43}];
